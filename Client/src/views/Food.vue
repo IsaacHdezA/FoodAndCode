44b22622308f8fe153f6a5465b5ee7dc81fa41bc
MacHip3r <mac_258@hotmail.com>
2021-06-09 22:07:02 -0500

    Correccion de colores (perdon diego)

--- Client/src/views/Food.vue
+++ Client/src/views/Food.vue
@@ -19,47 +19,47 @@
                     </v-col>
                     <v-col cols="9">
                       <v-text-field
-                          class="mb-n3"
-                          v-model="com_nombre"
-                          background-color="white"
-                          label="Nombre"
-                          solo
-                          outlined
-                          dense
-                          required
-                          ></v-text-field>
+                        class="mb-n3"
+                        v-model="com_nombre"
+                        background-color="white"
+                        label="Nombre"
+                        solo
+                        outlined
+                        dense
+                        required
+                      ></v-text-field>
                     </v-col>
                     <v-col cols="3" class="text-right mt-n6">
                       <h3 class="black--text">Categoría</h3>
                     </v-col>
                     <v-col cols="9">
                       <v-combobox
-                          class="my-n4"
-                          v-model="com_cat_id"
-                          :items="categorias"
-                          label="Seleccionar categoria"
-                          solo
-                          background-color="white"
-                          outlined
-                          dense
-                          ></v-combobox>
+                        class="my-n4"
+                        v-model="com_cat_id"
+                        :items="categorias"
+                        label="Seleccionar categoria"
+                        solo
+                        background-color="white"
+                        outlined
+                        dense
+                      ></v-combobox>
                     </v-col>
                     <v-col cols="3" class="text-right mt-n6">
                       <h3 class="black--text">Precio</h3>
                     </v-col>
                     <v-col cols="9">
                       <v-text-field
-                          class="mt-n3 mb-n6 pb-n6"
-                          v-model="com_precio"
-                          :rules="reglas_precio"
-                          prefix="$"
-                          label="Precio de la comida"
-                          solo
-                          background-color="white"
-                          outlined
-                          dense
-                          required
-                          ></v-text-field>
+                        class="mt-n3 mb-n6 pb-n6"
+                        v-model="com_precio"
+                        :rules="reglas_precio"
+                        prefix="$"
+                        label="Precio de la comida"
+                        solo
+                        background-color="white"
+                        outlined
+                        dense
+                        required
+                      ></v-text-field>
                     </v-col>
                   </v-row>
                 </v-col>
@@ -71,15 +71,15 @@
                     </v-col>
                     <v-col cols="9">
                       <v-textarea
-                          outlined
-                          rows="5"
-                          label="Descripción"
-                          solo
-                          v-model="com_descripcion"
-                          background-color="white"
-                          class="mb-n6 pb-n6"
-                          no-resize
-                          ></v-textarea>
+                        outlined
+                        rows="5"
+                        label="Descripción"
+                        solo
+                        v-model="com_descripcion"
+                        background-color="white"
+                        class="mb-n6 pb-n6"
+                        no-resize
+                      ></v-textarea>
                     </v-col>
                   </v-row>
                 </v-col>
@@ -87,14 +87,13 @@
                 <v-col cols="2">
                   <v-row justify="center">
                     <v-col cols="10">
-                      <v-btn class="px-7" color="accent2" rounded>Agregar</v-btn>
+                      <v-btn class="px-7" color="accent" rounded>Agregar</v-btn>
                     </v-col>
                   </v-row>
                 </v-col>
               </v-row>
             </v-form>
-          </template> 
-
+          </template>
         </v-card-text>
       </v-card>
     </template>
@@ -112,43 +111,41 @@
           hide-details
           ></v-text-field>
           </v-card-title>-->
-          <v-data-table
-              :headers="encabezados"
-              :items="platillos"
-              :search="search"
-              dense
-              >
+        <v-data-table
+          :headers="encabezados"
+          :items="platillos"
+          :search="search"
+          dense
+        >
+          <template v-slot:[`item.com_precio`]="{ item }">
+            <span
+              class="d-inline-block text-truncate mr-n16"
+              style="max-width: 250px;"
+            >
+              $ {{ item.com_precio }}
+            </span>
+          </template>
 
-              <template v-slot:[`item.com_precio`]="{ item }">
-                <span
-                    class="d-inline-block text-truncate mr-n16"
-                    style="max-width: 250px;"
-                    >
-                    $ {{ item.com_precio}}
-                </span>
-              </template>
+          <template v-slot:[`item.com_descripcion`]="{ item }">
+            <span
+              class="d-inline-block text-truncate mr-n16"
+              style="max-width: 250px;"
+            >
+              {{ item.com_descripcion }}
+            </span>
+          </template>
 
-            <template v-slot:[`item.com_descripcion`]="{ item }">
-              <span
-                  class="d-inline-block text-truncate mr-n16"
-                  style="max-width: 250px;"
-                  >
-                  {{ item.com_descripcion}}
-              </span>
-            </template>
-
-            <template v-slot:[`item.actions`]="{ item }">
-              <v-icon small class="mr-3" @click="editar_platillo(item)">
-                fas fa-pen
-              </v-icon>
-              <v-icon small @click="eliminar_platillo(item)">
-                fas fa-trash
-              </v-icon>
-            </template>
-          </v-data-table>
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon small class="mr-3" @click="editar_platillo(item)">
+              fas fa-pen
+            </v-icon>
+            <v-icon small @click="eliminar_platillo(item)">
+              fas fa-trash
+            </v-icon>
+          </template>
+        </v-data-table>
       </v-card>
     </template>
-
   </v-container>
 </template>
 
@@ -157,27 +154,24 @@ export default {
   name: "Food",
   data: () => ({
     valid: false,
-    com_nombre: '',
-    com_precio: '',
+    com_nombre: "",
+    com_precio: "",
     reglas_precio: [
-      v => /^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/.test(v) || 'Cantidad incorrecta',
+      (v) =>
+        /^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/.test(v) ||
+        "Cantidad incorrecta",
     ],
-    com_cat_id: '',
-    categorias: [
-      'Programming',
-      'Design',
-      'Vue',
-      'Vuetify',
-    ],
-    com_descripcion: '',
+    com_cat_id: "",
+    categorias: ["Programming", "Design", "Vue", "Vuetify"],
+    com_descripcion: "",
 
-    search: '',
+    search: "",
     encabezados: [
-      { text: 'Categoria', value: 'com_cat_id' },
-      { text: 'Nombre', value: 'com_nombre' },
-      { text: 'Precio', value: 'com_precio' },
-      { text: 'Descripción', value: 'com_descripcion' },
-      { text: 'Acciones', value: 'actions' },
+      { text: "Categoria", value: "com_cat_id" },
+      { text: "Nombre", value: "com_nombre" },
+      { text: "Precio", value: "com_precio" },
+      { text: "Descripción", value: "com_descripcion" },
+      { text: "Acciones", value: "actions" },
     ],
     platillos: [],
     nuevo_platillo: [],
@@ -187,20 +181,20 @@ export default {
   },
   methods: {
     async llenar_platillos() {
-      const api_data = await this.axios.get('/comida/menu');
+      const api_data = await this.axios.get("/comida/menu");
       this.platillos = api_data.data;
     },
-    async eliminar_platillo(item){
+    async eliminar_platillo(item) {
       const body = {
-        com_id: item.com_id
+        com_id: item.com_id,
       };
-      await this.axios.post('/comida/eliminar_platillo', body);
+      await this.axios.post("/comida/eliminar_platillo", body);
       this.llenar_platillos();
     },
     async guardar_platillo() {
-      await this.axios.post('/comida/nuevo_platillo', this.nuevo_platillo);
+      await this.axios.post("/comida/nuevo_platillo", this.nuevo_platillo);
       this.llenar_platillos();
-    }
+    },
   },
   components: {},
 };
@@ -211,9 +205,10 @@ th span {
   color: black;
 }
 tbody {
-  background-color: #FBFAF1;
+  background-color: #fbfaf1;
 }
-.v-data-table-header, .v-data-footer {
-  background-color: #F5F5F5;
+.v-data-table-header,
+.v-data-footer {
+  background-color: #f5f5f5;
 }
 </style>
