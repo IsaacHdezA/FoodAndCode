130d99b93293bcfab1a79945e4ba2fe20b1e3953
MacHip3r <mac_258@hotmail.com>
2021-06-09 22:01:10 -0500

    Colores en vuetify

--- Client/src/plugins/vuetify.js
+++ Client/src/plugins/vuetify.js
@@ -1,33 +1,33 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
-import es from 'vuetify/lib/locale/es';
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import es from "vuetify/lib/locale/es";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
-      options: {
-        customProperties: true,
-      },
+    options: {
+      customProperties: true,
+    },
     themes: {
       light: {
-        primary: '#2790BA',
-        secondary: '#EDE8CA',
-        accent: '#FBFAF1',
-        accent2: '#FECD48',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107'
+        primary: "#2790BA",
+        secondary: "#EDE8CA",
+        third: "#FBFAF1",
+        accent: "#FECD48",
+        error: "#FF5252",
+        info: "#6FB5D1",
+        success: "#4CAF50",
+        warning: "#FFC107",
       },
     },
   },
-    lang: {
-      locales: { es },
-      current: 'es',
-    },
+  lang: {
+    locales: { es },
+    current: "es",
+  },
   icons: {
-    iconfont: 'fa',
+    iconfont: "fa",
   },
 });
