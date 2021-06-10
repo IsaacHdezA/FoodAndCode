b61bf1a6c4031ebd95ea6382b4345aa4dc35e8c9
MacHip3r <mac_258@hotmail.com>
2021-06-10 18:39:12 -0500

    cambio formato de hora (query orden)

--- Server/models/order_model.js
+++ Server/models/order_model.js
@@ -3,7 +3,7 @@ const order = () => {};
 
 order.allOrders = (data, callback) =>
   connection.query(
-    "SELECT o.ord_id, t.mes_id, e.mro_nombre, o.ord_fecha_hora, SUM(so.sub_cant * f.com_precio) AS ord_precio FROM orden o INNER JOIN mesero e ON(e.mro_id = o.ord_mro_id) INNER JOIN mesa t ON(t.mes_id = o.ord_mes_id) INNER JOIN suborden so ON(so.sub_ord_id = o.ord_id) INNER JOIN comida f ON(f.com_id = so.sub_com_id) WHERE o.ord_estado = 'a' GROUP BY o.ord_id",
+    "SELECT o.ord_id, t.mes_id, e.mro_nombre, DATE_FORMAT(o.ord_fecha_hora, '%d/%m/%Y a las %H:%i') AS ord_fecha_hora, SUM(so.sub_cant * f.com_precio) AS ord_precio FROM orden o INNER JOIN mesero e ON(e.mro_id = o.ord_mro_id) INNER JOIN mesa t ON(t.mes_id = o.ord_mes_id) INNER JOIN suborden so ON(so.sub_ord_id = o.ord_id) INNER JOIN comida f ON(f.com_id = so.sub_com_id) WHERE o.ord_estado = 'a' GROUP BY o.ord_id",
     data,
     callback
   );
