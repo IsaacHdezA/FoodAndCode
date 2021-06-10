262d41bfa5fdc993cf9b258001bddcfd4ddf0922
MacHip3r <mac_258@hotmail.com>
2021-06-10 01:16:51 -0500

    Ruta orders en app.js

--- Server/app.js
+++ Server/app.js
@@ -1,28 +1,24 @@
-var express = require('express');
-var path = require('path');
-
-var morgan = require('morgan');
-
-
-var cors=require('cors');
+var express = require("express");
+var path = require("path");
+var morgan = require("morgan");
+var cors = require("cors");
 var app = express();
 
-//Rutas
-var mesero_ruta=require('./routes/mesero_ruta');
-var comida_ruta=require('./routes/comida_ruta');
+var mesero_ruta = require("./routes/mesero_ruta");
+var comida_ruta = require("./routes/comida_ruta");
+var order_route = require("./routes/order_route.js");
 
 app.use(cors());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "uploads")));
 
-app.use(express.static(path.join(__dirname, 'uploads')));
+app.use("/mesero", mesero_ruta);
+app.use("/comida", comida_ruta);
+app.use("/orders", order_route);
 
-
-app.use("/mesero",  mesero_ruta);
-app.use("/comida",  comida_ruta);
-app.get('/favicon.ico', (req, res) => res.status(204));
-
-
+app.get("/favicon.ico", (req, res) => res.status(204));
 
 module.exports = app;
