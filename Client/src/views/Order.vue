4151ac500b8c828c74d7aeb4a7e1360bd234132c
MacHip3r <mac_258@hotmail.com>
2021-06-10 18:24:29 -0500

    Visibilidad de ordenes

--- Client/src/views/Order.vue
+++ Client/src/views/Order.vue
@@ -13,6 +13,9 @@
             <v-row justify="center" align="center" align-content="center">
               <v-col cols="6">
                 <v-select
+                  flat
+                  solo-inverted
+                  hide-details
                   :items="employees"
                   v-model="newOrder.mro_id"
                   label="Mesero a cargo"
@@ -21,6 +24,9 @@
               </v-col>
               <v-col cols="4">
                 <v-select
+                  flat
+                  solo-inverted
+                  hide-details
                   :items="tables"
                   v-model="newOrder.mes_id"
                   label="Mesas disponibles"
@@ -52,25 +58,12 @@
     <template>
       <v-data-iterator
         :items="orders"
-        :items-per-page.sync="itemsPerPage"
-        :page.sync="page"
-        :search="search"
         :sort-by="sortBy.toLowerCase()"
-        :sort-desc="sortDesc"
         hide-default-footer
       >
         <template v-slot:header>
-          <v-toolbar dark color="blue darken-3" class="mb-1">
-            <v-text-field
-              v-model="search"
-              clearable
-              flat
-              solo-inverted
-              hide-details
-              prepend-inner-icon="mdi-magnify"
-              label="Search"
-            ></v-text-field>
-            <template v-if="$vuetify.breakpoint.mdAndUp">
+          <v-toolbar dark color="primary" class="mb-1 container-inside">
+            <template>
               <v-spacer></v-spacer>
               <v-select
                 v-model="sortBy"
@@ -82,14 +75,6 @@
                 label="Sort by"
               ></v-select>
               <v-spacer></v-spacer>
-              <v-btn-toggle v-model="sortDesc" mandatory>
-                <v-btn large depressed color="blue" :value="false">
-                  <v-icon>mdi-arrow-up</v-icon>
-                </v-btn>
-                <v-btn large depressed color="blue" :value="true">
-                  <v-icon>mdi-arrow-down</v-icon>
-                </v-btn>
-              </v-btn-toggle>
             </template>
           </v-toolbar>
         </template>
@@ -97,7 +82,7 @@
           <v-row>
             <v-col
               v-for="order in orders"
-              :key="order.name"
+              :key="order.ord_id"
               cols="12"
               sm="6"
               md="4"
@@ -105,82 +90,37 @@
             >
               <v-card>
                 <v-card-title class="subheading font-weight-bold">
-                  {{ order.name }}
+                  Mesa {{ order.mes_id }}
                 </v-card-title>
                 <v-divider></v-divider>
-                <v-list dense>
+                <v-list dense class="container-inside">
                   <v-list-item>
-                    <v-list-item-content>Calories:</v-list-item-content>
+                    <v-list-item-content>Mesero a cargo:</v-list-item-content>
                     <v-list-item-content class="align-end">
-                      {{ order.calories }}
+                      {{ order.mro_nombre }}
                     </v-list-item-content>
                   </v-list-item>
-
                   <v-list-item>
-                    <v-list-item-content>Fat:</v-list-item-content>
+                    <v-list-item-content>Fecha y hora:</v-list-item-content>
                     <v-list-item-content class="align-end">
-                      {{ order.fat }}
+                      {{ order.ord_fecha_hora }}
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-content>Estado:</v-list-item-content>
+                    <v-list-item-content class="align-end">
+                      Activa
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-content>Precio:</v-list-item-content>
+                    <v-list-item-content class="align-end">
+                      ${{ order.ord_precio }}
                     </v-list-item-content>
                   </v-list-item>
                 </v-list>
               </v-card>
             </v-col>
-          </v-row>
-        </template>
-        <template v-slot:footer>
-          <v-row class="mt-2" align="center" justify="center">
-            <span class="grey--text">Items per page</span>
-            <v-menu offset-y>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  dark
-                  text
-                  color="primary"
-                  class="ml-2"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  {{ itemsPerPage }}
-                  <v-icon>mdi-chevron-down</v-icon>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-list-item
-                  v-for="(number, index) in itemsPerPageArray"
-                  :key="index"
-                  @click="updateItemsPerPage(number)"
-                >
-                  <v-list-item-title>{{ number }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-
-            <v-spacer></v-spacer>
-
-            <span
-              class="mr-4
-            grey--text"
-            >
-              Page {{ page }} of {{ numberOfPages }}
-            </span>
-            <v-btn
-              fab
-              dark
-              color="blue darken-3"
-              class="mr-1"
-              @click="formerPage"
-            >
-              <v-icon>mdi-chevron-left</v-icon>
-            </v-btn>
-            <v-btn
-              fab
-              dark
-              color="blue darken-3"
-              class="ml-1"
-              @click="nextPage"
-            >
-              <v-icon>mdi-chevron-right</v-icon>
-            </v-btn>
           </v-row>
         </template>
       </v-data-iterator>
@@ -203,37 +143,11 @@ export default {
   name: "Order",
 
   data: () => ({
-    itemsPerPageArray: [4, 8, 12],
-    search: "",
     filter: {},
-    sortDesc: false,
-    page: 1,
-    itemsPerPage: 4,
     sortBy: "name",
     keys: ["Name", "Calories", "Fat"],
 
-    orders: [
-      {
-        name: "Frozen Yogurt",
-        calories: 159,
-        fat: 6.0,
-      },
-      {
-        name: "Ice cream sandwich",
-        calories: 237,
-        fat: 9.0,
-      },
-      {
-        name: "Eclair",
-        calories: 262,
-        fat: 16.0,
-      },
-      {
-        name: "Cupcake",
-        calories: 305,
-        fat: 3.7,
-      },
-    ],
+    orders: [],
     employees: [],
     tables: [],
     newOrder: {
