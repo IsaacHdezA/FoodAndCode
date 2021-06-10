c6888a14b0638e6ca9a48b982d76617670d5accf
MacHip3r <mac_258@hotmail.com>
2021-06-10 15:44:31 -0500

    Codigo simple

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,9 @@
-const app = require('./app');
-var http = require('http').Server(app);
-const PORT = process.env.PORT  || 3000 ;// Elige un puerto al azar si no puede escuchar el que se puso.
+const app = require("./app");
+var http = require("http").Server(app);
+const PORT = process.env.PORT || 3000; // Elige un puerto al azar si no puede escuchar el que se puso.
 
-http.listen( PORT, (err) => {
-    if(err) console.log("Hubo un error");
-    else console.log(`La api esta corriendo en puerto ${PORT}`);
-});
+http.listen(PORT, (err) =>
+  err
+    ? console.log("Hubo un error")
+    : console.log(`La api esta corriendo en puerto ${PORT}`)
+);
