b782613177ad7623fe9b403632f1314f4d723f42
MacHip3r <mac_258@hotmail.com>
2021-06-11 14:11:17 -0500

    ruta de la query showorderpertable

--- Server/routes/payment_route.js
+++ Server/routes/payment_route.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const paymentControl = require("../controllers/payment_control.js");
 
 router.get("/showAllPayments", paymentControl.showAllPayments);
+router.get(
+  "/showOrdersPerTable/:sub_ord_id",
+  paymentControl.showOrdersPerTable
+);
 router.get("/orderPerTable", paymentControl.showOrdersPerTable);
 router.get("/orderTotal", paymentControl.orderTotal);
 router.post("/insertPayment", paymentControl.insertPayment);
