4c427c5328da7e8d702fca2de2555cafcfabaa35
MacHip3r <mac_258@hotmail.com>
2021-06-13 04:29:08 -0500

    Avance diseño estadisticas

--- Client/src/views/Statistic.vue
+++ Client/src/views/Statistic.vue
@@ -4,12 +4,48 @@
     <br />
     <v-container>
       <div class="container-grid">
-        <div class="block container-earned-today"></div>
-        <div class="block container-profits"></div>
-        <div class="block container-orders-date"></div>
-        <div class="block container-sales-date"></div>
-        <div class="block container-orders-table"></div>
-        <div class="block container-orders-employee"></div>
+        <div class="block container-earned-today">
+          <v-container>
+            <v-row>
+              <h1>Hoy</h1>
+            </v-row>
+          </v-container>
+        </div>
+        <div class="block container-profits">
+          <v-container>
+            <v-row>
+              <h1>Ganancias</h1>
+            </v-row>
+          </v-container>
+        </div>
+        <div class="block container-orders-date">
+          <v-container>
+            <v-row>
+              <h1>Órdenes por fecha</h1>
+            </v-row>
+          </v-container>
+        </div>
+        <div class="block container-sales-date">
+          <v-container>
+            <v-row>
+              <h1>Ventas por fecha</h1>
+            </v-row>
+          </v-container>
+        </div>
+        <div class="block container-orders-table">
+          <v-container>
+            <v-row>
+              <h1>Órdenes por mesa</h1>
+            </v-row>
+          </v-container>
+        </div>
+        <div class="block container-orders-employee">
+          <v-container>
+            <v-row>
+              <h1>Órdenes por mesero</h1>
+            </v-row>
+          </v-container>
+        </div>
       </div>
     </v-container>
   </v-container>
