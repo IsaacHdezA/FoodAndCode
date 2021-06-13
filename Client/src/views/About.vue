d0770e8fdc8165f3744fc07eaf3f0b87e5f0255e
MacHip3r <mac_258@hotmail.com>
2021-06-12 19:10:01 -0500

    Vista about

---
+++ Client/src/views/About.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-container>
+    <h1>Acerca de</h1>
+    <br />
+    <template>
+      <v-row align="center" align-content="center" justify="center"></v-row>
+      <v-row align="center" align-content="center" justify="center">
+        <v-col cols="12" sm="12" md="6" lg="2">
+          <v-card>
+            <v-img
+              height="250"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+            <v-card-title>Aldo Isaac Hernández Antonio</v-card-title>
+            <v-card-text>
+              <div class="container-inside">
+                Small plates, salads & sandwiches - an intimate setting with 12
+                indoor seats plus patio seating.
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="12" md="6" lg="2">
+          <v-card>
+            <v-img
+              height="250"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+            <v-card-title>Braulio José Baca Barbosa</v-card-title>
+            <v-card-text>
+              <div class="container-inside">
+                Small plates, salads & sandwiches - an intimate setting with 12
+                indoor seats plus patio seating.
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="12" md="6" lg="2">
+          <v-card>
+            <v-img
+              height="250"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+            <v-card-title>Diego Armando Gutiérrez Ayala</v-card-title>
+            <v-card-text>
+              <div class="container-inside">
+                Small plates, salads & sandwiches - an intimate setting with 12
+                indoor seats plus patio seating.
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="12" md="6" lg="2">
+          <v-card>
+            <v-img
+              height="250"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+            <v-card-title>Héctor Guevara Mosqueda</v-card-title>
+            <v-card-text>
+              <div class="container-inside">
+                Small plates, salads & sandwiches - an intimate setting with 12
+                indoor seats plus patio seating.
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="12" md="6" lg="2">
+          <v-card>
+            <v-img
+              height="250"
+              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            ></v-img>
+            <v-card-title>Misael Ramírez Ramírez</v-card-title>
+            <v-card-text
+              >s
+              <div class="container-inside">
+                Small plates, salads & sandwiches - an intimate setting with 12
+                indoor seats plus patio seating.
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </v-container>
+</template>
+
+<style lang="css">
+@import "../styles/app.css";
+</style>
+
+<script>
+export default {
+  name: "About",
+
+  data: () => ({}),
+
+  watch: {},
+
+  created() {},
+
+  methods: {},
+
+  components: {},
+};
+</script>
