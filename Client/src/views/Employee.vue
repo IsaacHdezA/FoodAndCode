296c75d32e07401af857918a3e2bd236aabe2fdb
MacHip3r <mac_258@hotmail.com>
2021-06-13 23:45:27 -0500

    mejor diseño mesero

--- Client/src/views/Employee.vue
+++ Client/src/views/Employee.vue
@@ -15,116 +15,99 @@
         </v-toolbar>
         <v-card-text>
           <template>
-            <v-row>
-              <v-col cols="10">
-                <v-row>
-                  <v-col cols="3" class="text-right mt-3">
-                    <h3 class="black--text">Nombre</h3>
-                  </v-col>
-                  <v-col cols="9">
-                    <v-text-field
-                      class="mb-n8"
-                      v-model="nuevo_mesero.mro_nombre"
-                      background-color="white"
-                      label="Nombre"
-                      solo
-                      outlined
-                      dense
-                      required
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="3" class="text-right mt-3">
-                    <h3 class="black--text">Dirección</h3>
-                  </v-col>
-                  <v-col cols="9">
-                    <v-text-field
-                      class="mb-n8"
-                      v-model="nuevo_mesero.mro_direccion"
-                      background-color="white"
-                      label="Direccion"
-                      solo
-                      outlined
-                      dense
-                      required
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="3" class="text-right mt-3">
-                    <h3 class="black--text">Correo electrónico</h3>
-                  </v-col>
-                  <v-col cols="9">
-                    <v-text-field
-                      class="mb-n8"
-                      v-model="nuevo_mesero.mro_correo"
-                      background-color="white"
-                      label="Correo"
-                      solo
-                      outlined
-                      dense
-                      required
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="3" class="text-right mt-3">
-                    <h3 class="black--text">Numéro de teléfono</h3>
-                  </v-col>
-                  <v-col cols="9">
-                    <v-text-field
-                      class="mb-n8"
-                      v-model="nuevo_mesero.mro_telefono"
-                      background-color="white"
-                      label="Telefono"
-                      solo
-                      outlined
-                      dense
-                      required
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="3" class="text-right mt-3">
-                    <h3 class="black--text">Sueldo</h3>
-                  </v-col>
-                  <v-col cols="9">
-                    <v-text-field
-                      class="mb-n8"
-                      v-model="nuevo_mesero.mro_sueldo"
-                      background-color="white"
-                      label="0.00"
-                      solo
-                      outlined
-                      dense
-                      required
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="3" class="text-right mt-3">
-                    <h3 class="black--text">Foto</h3>
-                  </v-col>
-                  <v-col cols="9">
-                    <v-file-input
-                      class="mb-n5"
-                      label="Seleccione su archivo"
-                      filled
-                      v-model="nuevo_mesero.mro_foto"
-                      prepend-icon="fas fa-camera"
-                      dense
-                      solo
-                      outlined
-                    ></v-file-input>
-                  </v-col>
-                </v-row>
-              </v-col>
-
-              <v-col cols="2" align-self="center">
-                <v-row justify="center">
-                  <v-col cols="10">
+            <v-container>
+              <v-row>
+                <v-col cols="5">
+                  <v-text-field
+                    class="mb-n8"
+                    v-model="nuevo_mesero.mro_nombre"
+                    background-color="white"
+                    label="Nombre"
+                    solo
+                    outlined
+                    dense
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="5">
+                  <v-text-field
+                    class="mb-n8"
+                    v-model="nuevo_mesero.mro_direccion"
+                    background-color="white"
+                    label="Direccion"
+                    solo
+                    outlined
+                    dense
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="5">
+                  <v-text-field
+                    class="mb-n8"
+                    v-model="nuevo_mesero.mro_correo"
+                    background-color="white"
+                    label="Correo"
+                    solo
+                    outlined
+                    dense
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="5">
+                  <v-text-field
+                    class="mb-n8"
+                    v-model="nuevo_mesero.mro_telefono"
+                    background-color="white"
+                    label="Telefono"
+                    solo
+                    outlined
+                    dense
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="2" align-self="center">
+                  <v-row>
                     <v-btn
-                      v-on:click="submit()"
-                      class="px-30 font-weight-black"
+                      class="ml-16"
+                      dark
+                      large
                       color="accent"
-                      rounded
-                      >Agregar</v-btn
+                      @click="submit()"
                     >
-                  </v-col>
-                </v-row>
-              </v-col>
-            </v-row>
+                      Agregar
+                    </v-btn>
+                  </v-row>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="5">
+                  <v-text-field
+                    class="mb-n8"
+                    v-model="nuevo_mesero.mro_sueldo"
+                    background-color="white"
+                    label="0.00"
+                    solo
+                    outlined
+                    dense
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="5">
+                  <v-file-input
+                    class="mb-n5"
+                    label="Seleccione su archivo"
+                    filled
+                    v-model="nuevo_mesero.mro_foto"
+                    prepend-icon="fas fa-camera"
+                    dense
+                    solo
+                    outlined
+                  ></v-file-input>
+                </v-col>
+              </v-row>
+            </v-container>
           </template>
         </v-card-text>
       </v-card>
@@ -163,7 +146,7 @@
                       v-on:click="lanzarEditable(item)"
                       white
                       icon
-                      color="dark"
+                      color="primary"
                     >
                       <v-icon dark>
                         fas fa-pencil-alt
@@ -184,13 +167,20 @@
                         />
                       </v-avatar>
                     </p>
-                    <h4>{{ item.mro_nombre }}</h4>
-                    <p>{{ item.mro_telefono }}</p>
+                    <h2>{{ item.mro_nombre }}</h2>
+                    <h4>{{ item.mro_telefono }}</h4>
 
-                    <h3 class="green--text" v-if="item.mro_estado === 'a'">
+                    <v-chip
+                      class="ma-2"
+                      color="green"
+                      text-color="white"
+                      v-if="item.mro_estado === 'a'"
+                    >
                       Activo
-                    </h3>
-                    <h3 v-else class="red--text">Inactivo</h3>
+                    </v-chip>
+                    <v-chip class="ma-2" color="red" text-color="white" v-else>
+                      Inactivo
+                    </v-chip>
                   </v-card-text>
                 </v-card>
               </v-col>
@@ -517,7 +507,7 @@ export default {
         form1.append("mro_id", this.empleado_actualizar.mro_id);
         await this.axios
           .put("/mesero/actualizar", form1, {
-            headers: {"Content-Type": "multipart/form-data"},
+            headers: { "Content-Type": "multipart/form-data" },
           })
           .then((response) => {
             this.getMeseros();
@@ -579,7 +569,7 @@ export default {
 
       await this.axios
         .post("/mesero/nuevo", form, {
-          headers: {"Content-Type": "multipart/form-data"},
+          headers: { "Content-Type": "multipart/form-data" },
         })
         .then((response) => {
           this.getMeseros();
