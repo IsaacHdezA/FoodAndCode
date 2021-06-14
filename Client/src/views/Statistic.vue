b0fd36d55b36b3120d4af6a2c95bf0bfa914b2a1
MacHip3r <mac_258@hotmail.com>
2021-06-14 11:55:05 -0500

    propa quiado

--- Client/src/views/Statistic.vue
+++ Client/src/views/Statistic.vue
@@ -470,7 +470,6 @@ export default {
       { text: "Mesa", align: "center", value: "mes_id" },
       { text: "Hora", align: "center", value: "ord_fecha_hora" },
       { text: "Estado", align: "center", value: "ord_estado" },
-      { text: "Propina", align: "center", value: "pag_propina" },
     ],
     months: [
       "Ene",
